6bcb94714ea38e4cd38c88a93eb119fb79e8988b
anusharun12 <ravulanusha@gmail.com>
2020-04-02 20:28:39 +1100

    Added abc variables in third class

--- corona project/src/com/icici/loans/carloans/ThirdClass.java
+++ corona project/src/com/icici/loans/carloans/ThirdClass.java
@@ -1,11 +1,12 @@
 package com.icici.loans.carloans;
 
 public class ThirdClass {
-
+	int a=10, b=20,c;
 	public static void main(String[] args)
 	{
 		 System.out.println( "Hello");
 		 System.out.println("I am in Third class");
+		 
 	}
 	
 
